aabed15ea5c49ea105b3137003093d16daa9dd8d
rkverma77 <rkverma9730@gmail.com>
2025-09-01 14:38:29 +0530

    configured MongoDB and Basic Routing

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import connectDB from './config/dbConfig.js';
+import { PORT } from './config/serverConfig.js';
+import apiRouter from './routes/apiRoutes.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRouter);
+
+app.get('/ping', (req, res) => {
+  return res.status(StatusCodes.OK).json({ message: 'pong' });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port : ${PORT}`);
+  connectDB();
+});
