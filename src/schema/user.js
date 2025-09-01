78cfa597f30a33799eaf5eb022fa9ca4acd1d77a
rkverma77 <rkverma9730@gmail.com>
2025-09-01 15:10:12 +0530

    Added user schema with robohash

---
+++ src/schema/user.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is Required'],
+      unique: [true, 'Email already Exists'],
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address'
+      ]
+    },
+
+    password: {
+      type: String,
+      required: [true, 'Password is Required']
+    },
+
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: [true, 'User already exists'],
+      match: [
+        /^[a-zA-Z0-9_]+$/,
+        'Username can only contain letters, numbers, and underscores'
+      ]
+    },
+
+    avatar: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', function saveUser(next) {
+  const user = this;
+  user.avatar = `https://robohash.org/${user.username}`;
+  next();
+});
+
+const User = mongoose.Model('User', userSchema);
+export default User;
