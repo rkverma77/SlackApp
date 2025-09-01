378d665e1bfdca5e7e9c84a75d6aa11fb52b7ee6
rkverma77 <rkverma9730@gmail.com>
2025-09-01 15:58:53 +0530

    added crud mehtods

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,19 @@
+import User from '../schema/user.js';
+import crudRepository from './crudRepository.js';
+
+const userRepository = {
+  ...crudRepository(User),
+
+  getByEmail: async function (email) {
+    const user = await User.findOne({ email });
+    return user;
+  },
+
+  getByUserName: async function (username) {
+    const user = await User.findOne({ username });
+    return user;
+  }
+};
+
+export default userRepository ; 
+
